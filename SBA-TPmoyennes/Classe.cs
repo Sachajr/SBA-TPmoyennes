44fa64d00bb1ba91c8baefcedf576cc6c4c96a78
Sachajr <162965235+Sachajr@users.noreply.github.com>
2024-03-15 16:50:59 +0100

    Add files via upload

---
+++ SBA-TPmoyennes/Classe.cs
@@ -0,0 +1,55 @@
+﻿using TPMoyennes;
+
+class Classe
+{
+    public string nomClasse { get; private set; }
+    public List<Eleve> eleves { get; private set; }
+    public List<string> matieres { get; private set; }
+
+    public Classe(string nomClasse)
+    {
+        this.nomClasse = nomClasse;
+        eleves = new List<Eleve>();
+        matieres = new List<string>();
+    }
+
+    public void ajouterEleve(string prenom, string nom)
+    {
+        if (eleves.Count < 30)
+        {
+            eleves.Add(new Eleve(prenom, nom));
+
+        }
+    }
+
+    public void ajouterMatiere(string matiere)
+    {
+        if (matieres.Count < 10)
+        {
+            matieres.Add(matiere);
+
+        }
+    }
+
+    public float MoyenneMatiere(int matiere)
+    {
+        float total = 0;
+        int count = 0;
+        foreach (var eleve in eleves)
+        {
+            total += eleve.MoyenneMatiere(matiere);
+            count++;
+        }
+        return count > 0 ? total / count : 0;
+    }
+
+    public float MoyenneGeneral()
+    {
+        float total = 0;
+        foreach (var eleve in eleves)
+        {
+            total += eleve.MoyenneGeneral();
+        }
+        return eleves.Count > 0 ? total / eleves.Count : 0;
+    }
+}
---
+++ SBA-TPmoyennes/Eleve.cs
@@ -0,0 +1,50 @@
+﻿namespace TPMoyennes
+{
+    internal class Eleve
+    {
+        public string prenom { get; set; }
+        public string nom { get; set; }
+        public List<Note> notes { get; set; }
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            notes = new List<Note>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            if (notes.Count < 200)
+            {
+                notes.Add(note);
+            }
+        }
+
+        public float MoyenneMatiere(int matiere)
+        {
+            float total = 0;
+            int count = 0;
+            foreach (var n in notes)
+            {
+                if (n.matiere == matiere)
+                {
+                    total += n.note;
+                    count++;
+                }
+            }
+            return count > 0 ? total / count : 0;
+        }
+
+        public float MoyenneGeneral()
+        {
+            float total = 0;
+            foreach (var n in notes)
+            {
+                total += n.note;
+            }
+            return notes.Count > 0 ? total / notes.Count : 0;
+        }
+
+    }
+}
